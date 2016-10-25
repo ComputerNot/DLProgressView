5473643ebf2c0021795ee06e982776eec49d4fce
John <John@Network-2.local>
2016-10-25 14:51:19 +0800

    There is 1.0.3. shit

--- DLProgressView.podspec
+++ DLProgressView.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 s.name = 'DLProgressView'
-s.version = '1.0.2'
+s.version = '1.0.3'
 s.license = 'MIT'
 s.summary = 'DLProgressView in iOS.'
 s.homepage = 'https://github.com/ComputerNot/DLProgressView'
--- DLProgressView/DLProgressView.m
+++ DLProgressView/DLProgressView.m
@@ -20,12 +20,12 @@
 }
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect {
+ // Drawing code
+ }
+ */
 
 #pragma mark - Get
 - (UIView *)mainView
@@ -43,7 +43,8 @@
 {
     if (!_hudImageView) {
         _hudImageView = [[UIImageView alloc] initWithFrame:CGRectMake(15, 5, MAIN_VIEW_WIDTH-30, MAIN_VIEW_WIDTH-30)];
-        NSString *path = [[NSBundle mainBundle] pathForResource:@"loadinggif4" ofType:@"gif"];
+        NSBundle *myBundle = [NSBundle bundleForClass:[self class]];
+        NSString *path = [myBundle pathForResource:@"loadinggif4" ofType:@"gif"];
         NSData *data = [NSData dataWithContentsOfFile:path];
         _hudImageView.image = [UIImage jHUDImageWithSmallGIFData:data scale:1];
     }
@@ -55,36 +56,38 @@
     if (!_alertLabel) {
         _alertLabel = [[UILabel alloc] initWithFrame:CGRectMake(HORIZONTAL_MARGIN, MAIN_VIEW_WIDTH-30, MAIN_VIEW_WIDTH-HORIZONTAL_MARGIN*2, 25)];
         _alertLabel.textAlignment = NSTextAlignmentCenter;
-//        _alertLabel.textColor = [UIColor whiteColor];
+        //        _alertLabel.textColor = [UIColor whiteColor];
     }
     return _alertLabel;
 }
 
 #pragma mark - Set
-//- (void)setAnimationType:(HudAnimationType)animationType
-//{
-//    
-//}
+- (void)setGifName:(NSString *)gifName
+{
+    if (self.animationType == HudAnimationTypeGif) {
+        _gifName = gifName;
+        NSString *path = [[NSBundle mainBundle] pathForResource:gifName ofType:@"gif"];
+        NSData *data = [NSData dataWithContentsOfFile:path];
+        self.hudImageView.image = [UIImage jHUDImageWithSmallGIFData:data scale:1];
+    }
+}
 
 - (void)setSpinImage:(UIImage *)spinImage
 {
-    _spinImage = spinImage;
     if (self.animationType == HudAnimationTypeImageSpin) {
+        _spinImage = spinImage;
         self.hudImageView.image = spinImage;
         self.hudImageView.bounds = CGRectMake(0, 0, 50, 50);
-//        if (!_timer) {
-            _timer = [NSTimer scheduledTimerWithTimeInterval:.008 target:self selector:@selector(hudImageSpin) userInfo:nil repeats:YES];
-//        }
-
+        [self imageTimerCreate];
     }
 }
 
 - (void)setSwitchImages:(NSArray *)switchImages
 {
-    _switchImages = switchImages;
     if (self.animationType == HudAnimationTypeImageSwitch) {
+        _switchImages = switchImages;
         self.hudImageView.bounds = CGRectMake(0, 0, 50, 50);
-        _timer = [NSTimer scheduledTimerWithTimeInterval:.02 target:self selector:@selector(hudImageSwitch) userInfo:nil repeats:YES];
+        [self imageTimerCreate];
     }
 }
 #pragma mark - Control
@@ -95,10 +98,10 @@
 
 - (void)hide
 {
-    if (_timer.isValid) {
-        [_timer invalidate];
-    }
+    //    if (_timer.isValid) {
+    [_timer invalidate];
     _timer = nil;
+    //    }
     [self removeFromSuperview];
 }
 
@@ -125,25 +128,56 @@
     if (index == self.switchImages.count) {
         index = 0;
     }
-//    [UIView animateWithDuration:0.019 animations:^{
-        self.hudImageView.image = [self.switchImages objectAtIndex:index];
-//    }];
+    //    [UIView animateWithDuration:0.019 animations:^{
+    self.hudImageView.image = [self.switchImages objectAtIndex:index];
+    //    }];
     index = index + 1;//angle角度 double angle;
+}
+
+- (void)layoutSubviews{
+    [super layoutSubviews];
+    
+    UIView *parent = self.superview;
+    self.frame = parent.bounds;
+    self.mainView.center = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2 - 40);
+}
+
+- (void)imageTimerCreate
+{
+    //    if (_timer.isValid) {
+    [_timer invalidate];
+    _timer = nil;
+    //    }
+    
+    if (self.animationType == HudAnimationTypeImageSpin) {
+        _timer = [NSTimer scheduledTimerWithTimeInterval:.02 target:self selector:@selector(hudImageSpin) userInfo:nil repeats:YES];
+    }
+    else if (self.animationType == HudAnimationTypeImageSwitch){
+        _timer = [NSTimer scheduledTimerWithTimeInterval:.02 target:self selector:@selector(hudImageSwitch) userInfo:nil repeats:YES];
+    }
 }
 
 #pragma mark - + Method
 + (instancetype)addToView:(UIView *)view alertText:(NSString *)title animationType:(HudAnimationType)animationType
 {
     //添加progressView到view上
-    DLProgressView *progressView = [[DLProgressView alloc] initWithFrame:view.frame];
+    DLProgressView *progressView = [[DLProgressView alloc] initWithFrame:view.bounds];
     progressView.backgroundColor = [UIColor clearColor];
     [view addSubview:progressView];
     [progressView show];
-
+    
     //设置progressView属性
     progressView.alertLabel.text = title;
     progressView.animationType = animationType;
     return progressView;
 }
 
+
+- (void)dealloc
+{
+    //    if (_timer.isValid) {
+    [_timer invalidate];
+    //    }
+    _timer = nil;
+}
 @end
